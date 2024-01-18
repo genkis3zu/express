98a60d3f20d6f02143acf8497509a50847ee08d6
Genkis <79037367+genkis3zu@users.noreply.github.com>
2024-01-18 21:58:38 +0900

    add all express

--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,26 @@
 var express = require("express");
 var router = express.Router();
+const cors = require("cors"); // corsミドルウェアを追加
+require("dotenv").config();
 
-const responseObjectDataAll = {
-  textObject1: {
-    id: 1,
-    title: "note1 title",
-    subtitle: "note1 subtitle",
-    bodytext: "note1 body text",
-  },
-  textObject2: {
-    id: 2,
-    title: "note2 title",
-    subtitle: "note2 subtitle",
-    bodytext: "note2 body text",
-  },
-};
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
-/* GET home page.
- * @returns {Object[]} data
- * @returns {number}
- * @returns {string}
- * @returns {string}
- */
-router.get("/", function (req, res, next) {
-  res.json(responseObjectDataAll);
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get("/", async (req, res) => {
+  // データベース、コレクションを指定
+  const database = client.db("notes");
+  const notes = database.collection("notes");
+
+  // idが１のドキュメントを取得
+  const query = { id: 2 };
+  const note = await notes.findOne(query);
+
+  res.json(note);
 });
 
 module.exports = router;
